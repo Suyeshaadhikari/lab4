3d4e9807b9b3142f0986487fe34619c91651da84
Suyeshaadhikari <malika.adhikari@deerwalk.edu.np>
2016-07-17 15:58:03 +0545

    Add files via upload

---
+++ factorial_0514.cpp
@@ -0,0 +1,24 @@
+/*  Program: To find factorial of a given number
+ By : Malika Adhikari [0514]
+   # Date   : 7/17/2016
+
+  */
+#include<stdio.h>
+long int fact(int);
+ int main()
+ {
+ 	int n,x;
+ 	printf("Enter a number to get its factorial\n\a");
+ 	scanf("%d",&n);
+ 	x=fact(n);
+ 	printf("The Factorial of %d is %d",n,x);
+ 	return 1;
+ }
+ 
+ long int fact(int n)
+ {
+ if (n==1 || n==0)
+ return 1;
+ else
+ return (n*fact(n-1));	
+}
